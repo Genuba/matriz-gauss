71a7c2308a231f811cff1c9e8e191885640326dc
DavidPiracoca <you@example.com>
2018-04-24 19:36:04 -0500

    fin op

--- pruebas/Gauss/src/com/ownk/prueba/GausUtils.java
+++ pruebas/Gauss/src/com/ownk/prueba/GausUtils.java
@@ -24,8 +24,8 @@ public class GausUtils {
 		 * =================================
 		 */
 		for(int filas = 0;filas < cantidadFilas;filas++) {
-			float denominador = 1;
 			List<Float> nodeMatriz = matriz.get(filas);
+			float denominador = 1;
 			/*
 			 * ================================
 			 * for que itera las columnas de 
@@ -44,9 +44,6 @@ public class GausUtils {
 			 * =================================
 			 */
 			for(int columnas = 0;columnas < cantidadColumnas;columnas++) {
-				if(filas == columnas && nodeMatriz.get(columnas) == 0) {
-					nodeMatriz.set(columnas,new Float(1));
-				}
 				if(denominador != 0){
 					float uno = nodeMatriz.get(columnas)/denominador;
 					nodeMatriz.set(columnas,uno);
@@ -60,13 +57,18 @@ public class GausUtils {
 			 * filas 1 y 2, 2 y 3, 4 y 5..
 			 * ==============================================
 			 */
+			float tempInferior = 0,tempSuperior = 0;	
 			for(int filasReduccion = (filas + 1);filasReduccion < cantidadFilas;filasReduccion++){
 				List<Float> nodeMatrizReduccion = matriz.get(filasReduccion);
 				for(int columnasreduccion = filas;columnasreduccion < cantidadColumnas;columnasreduccion++){
-					float inferior = nodeMatrizReduccion.get(columnasreduccion)*nodeMatriz.get(filas);
-					float superior = nodeMatrizReduccion.get(columnasreduccion)*nodeMatriz.get(columnasreduccion);
-					float temp = inferior - superior;
-					nodeMatrizReduccion.set(columnasreduccion,temp);
+					if (columnasreduccion == filas) {
+						tempSuperior = nodeMatrizReduccion.get(filas);
+						tempInferior = nodeMatriz.get(filas);
+					}
+					float inferior = nodeMatrizReduccion.get(columnasreduccion)*tempInferior;
+					float superior = nodeMatriz.get(columnasreduccion)*tempSuperior; 
+					float ope = (-1)*superior + inferior;
+					nodeMatrizReduccion.set(columnasreduccion,ope);
 				}
 			}
 		}
@@ -98,13 +100,18 @@ public class GausUtils {
 			 * entre filas 4 y 5, 2 y 3, 1 y 2 
 			 * ==============================================
 			 */
+			float tempInferior = 0,tempSuperior = 0;
 			for(int filasReduccion = (filas - 1);filasReduccion >= 0;filasReduccion--){
 				List<Float> nodeMatrizReduccion = matriz.get(filasReduccion);
-				for(int columnasreduccion = (filas - 1);columnasreduccion < cantidadColumnas;columnasreduccion++){
-					float inferior = nodeMatrizReduccion.get(columnasreduccion)*nodeMatriz.get(filas);
-					float superior = nodeMatrizReduccion.get(columnasreduccion)*nodeMatriz.get(columnasreduccion);
-					float temp = inferior - superior;
-					nodeMatrizReduccion.set(columnasreduccion,temp);
+				for(int columnasreduccion = filas;columnasreduccion < cantidadColumnas;columnasreduccion++){
+					if (columnasreduccion == filas) {
+						tempSuperior = nodeMatrizReduccion.get(filas);
+						tempInferior = nodeMatriz.get(filas);
+					}
+					float inferior = nodeMatrizReduccion.get(columnasreduccion)*tempInferior;
+					float superior = nodeMatriz.get(columnasreduccion)*tempSuperior; 
+					float ope = (-1)*superior + inferior;
+					nodeMatrizReduccion.set(columnasreduccion,ope);
 				}
 			}
 		}
